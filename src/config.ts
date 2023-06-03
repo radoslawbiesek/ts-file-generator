4bc4d6be0023a5ac9a5df13c6226b4212d8afb34
Radosław Biesek <radoslaw.biesek@gmail.com>
2023-06-03 08:31:56 +0200

    validate config

--- src/config.ts
+++ src/config.ts
@@ -1,19 +1,55 @@
-import { z } from "zod";
+import { ZodError, z } from "zod";
 
 const configSchema = z.array(
   z.object({
     name: z.string(),
     alias: z.string().optional(),
-    template: z.union([
-      z.string(),
-      z.function().args(z.string().optional()).returns(z.string()),
-    ]),
+    files: z.array(
+      z.object({
+        template: z.union([
+          z.string(),
+          z.function().args(z.string().optional()).returns(z.string()),
+        ]),
+        path: z.union([
+          z.string(),
+          z.function().args(z.string().optional()).returns(z.string()),
+        ]),
+      })
+    ),
   })
 );
 
-export async function getConfig(path: string) {
-  const configRaw = await import(path);
-  const config = configSchema.parse(configRaw.default);
+type Config = {
+  name: string;
+  alias?: string;
+  files: {
+    template: string | ((name: string) => string);
+    path: string | ((name: string) => string);
+  }[];
+}[];
 
-  return config;
+export async function getConfig(path: string): Promise<Config> {
+  const configRaw = await import(path);
+  const result = configSchema.safeParse(configRaw.default);
+
+  if (!result.success) {
+    throw new Error(createErrorMsg(result.error));
+  }
+
+  return result.data;
+}
+
+function createErrorMsg(err: ZodError) {
+  return `Invalid config format. Please make sure the provided config meets the following type:
+
+  type Config = {
+    name: string;
+    alias?: string;
+    files: {
+      template: string | ((name: string) => string);
+      path: string | ((name: string) => string);
+    }[];
+  }[];
+  
+  Validation error: ${err}`;
 }
--- src/tgconfig.cjs
+++ src/tgconfig.cjs
@@ -2,6 +2,20 @@ module.exports = [
     {
         name: 'component',
         alias: "c",
-        template: () => "hello"
+        files: [
+            {
+                path: (name) => `src/components/${name}.tsx`,
+                template: (name) => `import React from 'react';
+
+                import './${name}.scss';
+
+                type ${name}Props {}
+
+                export function ${name}({}: ${name}Props) {
+                  return <div>${name} component works.</div>;
+                };
+                `
+            }
+        ]
     }
 ]
